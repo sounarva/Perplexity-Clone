8f2daec05fc92571847148c94ada4e5e0f085507
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-03-21 00:24:55 +0530

    Mail service issue fixed

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -8,7 +8,8 @@ const transporter = nodemailer.createTransport({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    }
+    },
+    family: 4
 })
 
 transporter.verify()
