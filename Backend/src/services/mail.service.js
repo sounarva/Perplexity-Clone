5e3365ac2346aa0ede969ed278960f67c41d29b1
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-03-21 01:03:25 +0530

    Bug Fixed 04

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         type: 'OAuth2',
         user: process.env.GOOGLE_USER,
